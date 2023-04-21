0b437a0d09f5fb78f389eb6ed543fa02ed1f6019
Konsttantin <Ryabovkostya91@icloud.com>
2023-04-22 00:28:53 +0300

    add tasks of seminar

---
+++ task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+int a = ReadInt("длина стороны А");
+int b = ReadInt("длина стороны Б");
+int c = ReadInt("длина стороны В");
+Treangle(a, b, c);
+
+int ReadInt(string argument)
+{
+    int number;
+    System.Console.WriteLine($"Введите {argument}: ");
+    while(!int.TryParse(Console.ReadLine(), out number) && number > 0)
+    {
+        System.Console.WriteLine("Не корректный ввод.");
+    }
+    return number;
+}
+
+void Treangle(int a, int b, int c)
+{
+    if( a + b > c && b + c > a && c + a > b)
+    {
+        System.Console.WriteLine("Треугольник с такими сторонами может существовать");
+    }
+    else
+    {
+        System.Console.WriteLine("Треугольник с такими сторонами не может существовать");
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int a = ReadInt("a number > 0");
+Binar(a);
+
+int ReadInt(string argument)
+{
+    int number;
+    System.Console.WriteLine($"Input {argument}: ");
+    while(!int.TryParse(Console.ReadLine(), out number) && number > 0)
+    {
+        System.Console.WriteLine("It's not that");
+    }
+    return number;
+}
+
+void Binar(int number)
+{
+    string line = string.Empty;
+    while(number > 0)
+    {
+        line = line + (number % 2).ToString();
+        number /=2;
+    }
+//System.Console.WriteLine(line);
+string temp = string.Empty;
+for (int i = 0; i < line.Length; i++)
+    {
+        temp += line[line.Length - 1 - i];
+    }
+    System.Console.WriteLine(temp);
+}
